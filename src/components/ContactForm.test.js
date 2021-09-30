a808118aea62388285b7090b411b9c972e215e82
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-09-30 12:25:16 -0700

    completed tests 1-3

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,41 +1,48 @@
-import React from 'react';
-import {render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import ContactForm from './ContactForm';
+import ContactForm from "./ContactForm";
 
-test('renders without errors', ()=>{
-    
+test("renders without errors", () => {
+  render(<ContactForm />);
 });
 
-test('renders the contact form header', ()=> {
-    
+test("renders the contact form header", () => {
+  // Arrange (render the form)
+  render(<ContactForm />);
+
+  // Act/Assert (look for and expect presense of header text in form)
+  const formHeaderText = screen.queryByText(/contact form/i);
+  expect(formHeaderText).toBeInTheDocument();
 });
 
-test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
+  // Arrange (render the form)
+  render(<ContactForm />);
+
+  // Act (user types only 3 letters inside firstname input)
+  const testInput = "abc";
+  const firstnameInput = screen.getByPlaceholderText(/edd/i);
+  userEvent.type(firstnameInput, testInput);
+
+  // Assert (is the error message for firstname present)
+  await waitFor(() => {
+    const firstnameError = screen.queryByText(
+      /firstname must have at least 5 characters./i
+    );
+    expect(firstnameError).toBeInTheDocument();
+  });
 });
 
-test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
-});
+test("renders THREE error messages if user enters no values into any fields.", async () => {});
 
-test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
-});
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
 
-test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
-});
+test('renders "email must be a valid email address" if an invalid email is entered', async () => {});
 
-test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
-});
+test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {});
 
-test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
-});
+test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {});
 
-test('renders all fields text when all fields are submitted.', async () => {
-    
-});
+test("renders all fields text when all fields are submitted.", async () => {});
