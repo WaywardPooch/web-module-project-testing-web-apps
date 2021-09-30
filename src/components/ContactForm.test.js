960ed3e0a4968a1b2309c1b92ae4f1b544f3a064
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-09-30 13:39:58 -0700

    completed test 8

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -44,9 +44,7 @@ test("renders THREE error messages if user enters no values into any fields.", a
   userEvent.click(submitButton);
 
   // Assert (are there three error messages showing up)
-  const minLettersError = screen.getByText(
-    /must have at least 5 characters./i
-  );
+  const minLettersError = screen.getByText(/must have at least 5 characters./i);
   const validEmailError = screen.getByText(/must be a valid email address./i);
   const requiredFieldError = screen.getByText(/is a required field./i);
 
@@ -80,9 +78,7 @@ test('renders "email must be a valid email address" if an invalid email is enter
 
   // Act (user fills in email input field with invalid email)
   const testValue = "notAnEmail";
-  const emailInput = screen.getByPlaceholderText(
-    /bluebill1049@hotmail.com/i
-  );
+  const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
   userEvent.type(emailInput, testValue);
 
   // Assert (is the invalid email error popping up)
@@ -103,6 +99,36 @@ test('renders "lastName is a required field" if an last name is not entered and 
   expect(lastnameError).toBeInTheDocument();
 });
 
-test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {});
+test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {
+  // Arrange (render the form)
+  render(<ContactForm />);
+
+  // Act (user fills in all fields but message and clicks submit)
+  const testValues = {
+    firstname: "Johnny",
+    lastname: "Snow",
+    email: "lordcommander@castleblack.com",
+  };
+  const firstnameInput = screen.getByPlaceholderText(/edd/i);
+  const lastnameInput = screen.getByPlaceholderText(/burke/i);
+  const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+  const submitButton = screen.getByRole("button");
+
+  userEvent.type(firstnameInput, testValues.firstname);
+  userEvent.type(lastnameInput, testValues.lastname);
+  userEvent.type(emailInput, testValues.email);
+  userEvent.click(submitButton);
+
+  // Assert (check if testValues are on screen, and if message is missing)
+  const submittedFirstname = screen.getByTestId("firstnameDisplay");
+  const submittedLastname = screen.getByTestId("lastnameDisplay");
+  const submittedEmail = screen.getByTestId("emailDisplay");
+  const submittedMessage = screen.queryByTestId("messageDisplay");
+
+  expect(submittedFirstname).toBeInTheDocument();
+  expect(submittedLastname).toBeInTheDocument();
+  expect(submittedEmail).toBeInTheDocument();
+  expect(submittedMessage).not.toBeInTheDocument();
+});
 
 test("renders all fields text when all fields are submitted.", async () => {});
